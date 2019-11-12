0ff8fe05d09958c6c46b3ec01476d05b3a7395f6
Serhii Melnyk <serhii_melnyk8@gmail.com>
2019-11-12 22:40:07 +0200

    set max lenght for input

--- src/components/form-candidates/form-candidates.js
+++ src/components/form-candidates/form-candidates.js
@@ -26,7 +26,7 @@ class FormCandidates {
           $div.append(`
             <div class="form-candidates__name-item">
               <label for=${i}>Альтернатива ${i + 1}</label>
-              <input id=${i} class="form-candidates__name-input ${SETTINGS.SELECTOR.INPUT_NAME}" type="text" required>
+              <input id=${i} class="form-candidates__name-input ${SETTINGS.SELECTOR.INPUT_NAME}" type="text" required maxlength="50">
             </div>
             `);
         }
