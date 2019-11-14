4621a8710102ab96c5e3a13c45979c9788bcc52b
Serhii Melnyk <serhii_melnyk8@gmail.com>
2019-11-14 11:08:17 +0200

    stylink code

--- webpack.config.js
+++ webpack.config.js
@@ -51,8 +51,8 @@ module.exports = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
-      })
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
   ]
 }
