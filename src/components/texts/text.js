9af8ed4bb113579e20b169677fcb85a90f099e30
Kruvyz <serhiimelnyk8@gmail.com>
2021-11-25 13:43:50 +0200

    another changes

--- src/components/texts/text.js
+++ src/components/texts/text.js
@@ -3,8 +3,8 @@ import { getFromStorage } from '../../service/data';
 export function RenderText() {
     const { experts, candidates, name, date } = JSON.parse($('#data').text());
 
-    $('.js-voting-name').text(`Результати колективної експертизи ${name}`);
-    $('.js-voting-data').text(date);
+    $('.js-voting-name').text(name);
+    $('.js-voting-data').text(date.replace('о ', ''));
     $('.js-candidates-count').text(`Кількість оцінюваних альтернатив: ${candidates.length}`);
     $('.js-experts-count').text(`Загальна кількість експертів: ${experts.length}`);    
 }
